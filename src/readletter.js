3eea8b24fc2d2d706224df6d07ac5adc76f67d7c
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-10 15:58:17 -0500

    Added recognition for accented vowels

--- src/readletter.js
+++ src/readletter.js
@@ -1,6 +1,7 @@
 // @flow
-
 const { VOWEL } = require('./models/vowels')
+const { A_GRAVE, E_GRAVE, I_GRAVE, O_GRAVE, U_GRAVE, E_ACUTE }  = require('./constants')
+
 const readLetter = (letter: string): string => {
   if(letter.length !== 1) {
     const msg = 'Letter cannot have length ' + letter.length
@@ -18,6 +19,19 @@ const readLetter = (letter: string): string => {
       return VOWEL.O
     case 'u':
       return VOWEL.U
+
+    case A_GRAVE:
+      return VOWEL.A_ACCENT_GRAVE
+    case E_GRAVE:
+      return VOWEL.E_ACCENT_GRAVE
+    case I_GRAVE:
+      return VOWEL.I_ACCENT_GRAVE
+    case O_GRAVE:
+      return VOWEL.O_ACCENT_GRAVE
+    case U_GRAVE:
+      return VOWEL.U_ACCENT_GRAVE
+    case E_ACUTE:
+      return VOWEL.E_ACCENT_ACUTE
     default:
       const msg = letter + ' is unrecognized'
       throw new Error(msg)
--- src/readletter.spec.js
+++ src/readletter.spec.js
@@ -1,3 +1,6 @@
+// @flow
+
+const Constants = require('./constants')
 const { readLetter } = require('./readletter')
 
 describe.only('readLetter', () => {
@@ -23,6 +26,18 @@ describe.only('readLetter', () => {
     it('should recognize the vowel: ' + vowel, () => {
       const read = readLetter(vowel)
       expect(read).toEqual(vowel)
+    })
+  })
+
+  const accented_vowels = [
+    Constants.A_GRAVE, Constants.E_GRAVE, Constants.I_GRAVE,
+    Constants.O_GRAVE, Constants.U_GRAVE, Constants.E_ACUTE
+  ]
+
+  accented_vowels.forEach(accented_vowel => {
+    it('should recognize the accented vowel: ' + accented_vowel, () => {
+      const read = readLetter(accented_vowel)
+      expect(read).toEqual(accented_vowel)
     })
   })
 })
