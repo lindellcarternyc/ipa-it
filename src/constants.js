893e71e0af0f4360d7b3c6998c4e9488b50caead
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-10 15:37:30 -0500

    Added constants for accented vowels

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const A_GRAVE = 'à'
+const E_GRAVE = 'è'
+const I_GRAVE = 'ì'
+const O_GRAVE = 'ò'
+const U_GRAVE = 'ù'
+
+const E_ACUTE = 'é'
+
+module.exports = {
+  A_GRAVE, E_GRAVE, I_GRAVE,
+  O_GRAVE, U_GRAVE, E_ACUTE
+}
