571d31d283de4e7ef2693561b4831274cc3780a4
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-10 22:12:27 -0500

    Ensured rejection of anything than a single char

--- src/letter/utils.js
+++ src/letter/utils.js
@@ -4,6 +4,10 @@ const { ACCENTED_VOWELS } = require('../constants')
 const { CONSONANT } = require('../models/consonants')
 
 const isVowel = (letter: string): boolean => {
+  if (letter.length !== 1) {
+    const errorMsg = 'A Letter must have a length of exactly 1, not ' + letter.length
+    throw new Error(errorMsg)
+  }
   if ('aeiou'.split('').indexOf(letter) >= 0) {
     return true
   } else if (ACCENTED_VOWELS.indexOf(letter) >= 0) {
