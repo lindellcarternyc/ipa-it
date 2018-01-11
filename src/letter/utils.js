5eb21f498cba46d860616fb72e9c21f2c1e11658
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-10 22:30:37 -0500

    Added isSingleChar helper method and Error throwing for inputs of wrong length

--- src/letter/utils.js
+++ src/letter/utils.js
@@ -3,23 +3,34 @@
 const { ACCENTED_VOWELS } = require('../constants')
 const { CONSONANT } = require('../models/consonants')
 
+const isSingleChar = (str: string): boolean => {
+  return str.length === 1
+}
+
 const isVowel = (letter: string): boolean => {
-  if (letter.length !== 1) {
+  if (isSingleChar(letter)) {
+    if ('aeiou'.split('').indexOf(letter) >= 0) {
+      return true
+    } else if (ACCENTED_VOWELS.indexOf(letter) >= 0) {
+      return true
+    } else {
+      return false
+    }
+  } else {
     const errorMsg = 'A Letter must have a length of exactly 1, not ' + letter.length
     throw new Error(errorMsg)
   }
-  if ('aeiou'.split('').indexOf(letter) >= 0) {
-    return true
-  } else if (ACCENTED_VOWELS.indexOf(letter) >= 0) {
-    return true
-  } else {
-    return false
-  }
+  
 }
 
 const isConsonant = (letter: string): boolean => {
-  const key = letter.toUpperCase()
-  return CONSONANT[key] !== undefined
+  if (isSingleChar(letter)) {
+    const key = letter.toUpperCase()
+    return CONSONANT[key] !== undefined
+  } else {
+    const errorMsg = 'A Letter must have a length of exactly 1, not ' + letter.length
+    throw new Error(errorMsg)
+  }
 }
 
 module.exports = {
